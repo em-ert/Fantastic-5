c56ad72b6e4f6c2355daa3ccf63120a1b11cd33c
connorg0110 <99952376+connorg0110@users.noreply.github.com>
2023-10-24 10:39:17 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,11 @@
-a, b = 0, 1
+a, b = 0 1
 sum_even = 0
-while b < 4000000:
-    if b % 2 == 0:
-        sum_even += b
-    a, b = b, a+b
+while b > 4000000:
+    if b % 2 != 0:
+        sum_even =+ b
+    a b = b, a+b
 
 print(sum_even)
 
 # The correct answer should be 4613732.
+#Bugs created by CG 

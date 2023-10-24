4f25f53e179fc6f7b880eb3a2b54c230798e1dff
clamosse <123004572+clamosse@users.noreply.github.com>
2023-10-24 10:44:50 -0400

    bugs introduced

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,10 +1,12 @@
+#bugs introduced: CL
+
 from datetime import datetime
 
 #y-m-d input date
-date_str = "2022-03-17 10:45:30"
+date_str = "10:45:30 2022-03-17"
 
 #makes y-m-d date
-date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+date_obj = datetime.strptime(date_str, '%m-%Y-%d %H:%M:%S')
 
 #reformats date to m-d-y
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')

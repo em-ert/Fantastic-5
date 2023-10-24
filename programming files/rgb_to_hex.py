0d2c2f1bf9e9f65e39487faab951e97b66766b4f
UVMRyan <122762150+UVMRyan@users.noreply.github.com>
2023-10-24 10:42:01 -0400

    Introduced bugs to rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,10 @@
+# introduced bugs: ryan langlois
+
 def rgb_to_hex(r, g, b):
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    return '{:01X}{:01X}{:01X}'.format(r, g, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"

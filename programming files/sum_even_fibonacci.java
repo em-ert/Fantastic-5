d9816403d67a90746631fee93e1f3b3ffd2b3329
connorg0110 <99952376+connorg0110@users.noreply.github.com>
2023-10-24 10:42:08 -0400

    Bugged up sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,16 +1,15 @@
 public class sum_even_fibonacci {
     public static void main(String[] args) {
-        int a = 0, b = 1;
+        int a = 0 b = 1;
         int sumEven = 0;
-        while (b < 4000000) {
-            if (b % 2 == 0) {
-                sumEven += b;
-            }
+        while (b > 4000000) 
+            if (b % 2 != 0) {
+                sumEven =+ b;
+            
             int temp = b;
-            b = a + b;
-            a = temp;
-        }
-        System.out.println(sumEven);
+            b = a + b, a = temp;
+        
+        System.out.println(sumOdd);
     }
 }
 

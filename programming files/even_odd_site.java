caa7c0f6bc7a6f01b7f961c045b4bd99725ff5aa
samdsw <106113432+samdsw@users.noreply.github.com>
2023-10-24 10:41:16 -0400

    Added bugs to even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -4,12 +4,12 @@ import java.io.*;
 public class even_odd_site {
     public static void main(String[] args) throws IOException {
         // Creates object to write to html file
-        BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
+         BufferReader file = new BufferedWriter(new FileWriter("numbers.html"));
         // Adds html code to file
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
         // Loops 50 times to add table row headers to file
-        for (int i = 1; i <= 50; i++) {
+        for (int i = 1.0; i <= 50; i++) {
             if (i % 2 == 0) {
                 file.write("<tr><td>" + i + "</td><td></td></tr>\n");
             }
